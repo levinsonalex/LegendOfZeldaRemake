58eff42d17bf7b29b9f8257c7f160fc60ae90cf4
ajbaines <antoine.baines@gmail.com>
2016-01-24 21:11:15 -0500

    Added Enemy Base Class

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum Mode{
+	walking, attacked
+};
+
+public class enemy : MonoBehaviour {
+
+	Random random = new Random();
+
+	int 				movingSpeed = 2;
+	int 				attackedSpeed = 3;
+	int					hitCount = 2;
+
+	public Direction 	curDirection;
+	public int 			curSpeed;
+	Mode				curMode;
+
+	Rigidbody			rig;
+
+	// Use this for initialization
+	void Start () {
+		curSpeed = movingSpeed;
+		rig = GetComponent<Rigidbody> ();
+		//change later
+		curDirection = Direction.NORTH;
+		curMode = Mode.walking;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 newVel = Vector3.zero;
+		if (curMode == Mode.walking) { 
+			if (curDirection == Direction.NORTH) {
+				newVel = curSpeed * Vector3.up;
+			} else if (curDirection == Direction.EAST) {
+				newVel = curSpeed * Vector3.right;
+			} else if (curDirection == Direction.SOUTH) {
+				newVel = curSpeed * Vector3.down;
+			} else if (curDirection == Direction.WEST) {
+				newVel = curSpeed * Vector3.left;
+			}
+		} else if (curMode == Mode.attacked) {
+			if (curDirection == Direction.NORTH) {
+				newVel = attackedSpeed * Vector3.down;
+			} else if (curDirection == Direction.EAST) {
+				newVel = attackedSpeed * Vector3.left;
+			} else if (curDirection == Direction.SOUTH) {
+				newVel = attackedSpeed * Vector3.up;
+			} else if (curDirection == Direction.WEST) {
+				newVel = attackedSpeed * Vector3.right;
+			}
+		}
+
+		rig.velocity = newVel;
+
+	
+	}
+
+	void OnCollisionEnter(Collision coll){
+		print ("Touched " + coll.transform.parent.name);
+		if (coll.gameObject.tag == "link") {
+			//take away a heart from link
+		} else if (coll.gameObject.tag == "weapon") {
+			//hit by the sword
+			hitCount--;
+			curMode = Mode.attacked;   
+		} else if (coll.transform.parent.name == "MapAnchor") {
+			print ("Anchor");
+			//switchDirections(curDirection);
+
+		}
+	}
+
+	void switchDirections(Direction oldDir){
+		while (curDirection == oldDir) {
+			curDirection = (Direction)Random.Range (0, 3);
+		}
+
+	}
+}
